45d16bfc48600327005a1db89716dea52c0e357f
Chigg <higgins.colin@gmail.com>
2018-06-07 19:39:53 +0000

    cleaned up, ready to add API check

--- tweepy_followers.py
+++ tweepy_followers.py
@@ -18,8 +18,8 @@ import re
 keys = []
 inFile = open("/home/higgins_colin/tweepy_followers/keys", "r")
 for line in inFile:
-	line = line.strip()
-	keys.append(line)
+        line = line.strip()
+        keys.append(line)
 
 consumer_key = str(keys[0])
 consumer_secret = str(keys[1])
@@ -36,163 +36,167 @@ api = tweepy.API(auth)
 
 def get_followers(username):
 
-	ids = []
-	i = 0
+        ids = []
+        i = 0
 
-	#finds follower ids
-	for page in tweepy.Cursor(api.followers_ids, screen_name = username).pages():
+        #finds follower ids
+        for page in tweepy.Cursor(api.followers_ids, screen_name = username).pages():
 
-		ids.extend(page)
-		i += 1
-		print("getting followers: %s" %(len(ids)))
+                ids.extend(page)
+                i += 1
+                print("getting followers: %s" %(len(ids)))
 
-		#only collect up to 1000
-		if len(ids) >= 1000:
-				break
+                #only collect up to 1000
+                if len(ids) >= 500:
+                                break
 
-		#3 queries then 5 min break, 500 users per query
-		if i == 3:
-			print("Avoiding rate_limit. Wait 5 mins.")
-			time.sleep(300)
-			i = 0
+                #3 queries then 5 min break, 500 users per query
+                if i == 3:
+                        print("Avoiding rate_limit. Wait 5 mins.")
+                        time.sleep(300)
+                        i = 0
 
-	#prints total gathered followers
-	print("Follower count gathered: ", len(ids))
-	return(ids)
+        #prints total gathered followers
+        print("Follower count gathered: ", len(ids))
+        return(ids)
 
 def check_connectivity(ids, original_user):
 
-	downtime = 0
-	i = 0
-	out_list = []
-	centrality_list = []
+        downtime = 0
+        i = 0
+        out_list = []
+        centrality_list = []
 
-	#shows writing to a new file
-	print ("writing to {0}_followers_tweets.csv".format(original_user))
-	with open("{0}_followers.csv".format(original_user) , 'w') as file:
-		writer = csv.writer(file, delimiter= '|')
+        #shows writing to a new file
+        print ("writing to {0}_followers_tweets.csv".format(original_user))
+        with open("{0}_followers.csv".format(original_user) , 'w') as file:
+                writer = csv.writer(file, delimiter= '|')
 
-		#start with original user's followers' ids
-		out_list = ids
-		out_list.insert(0, original_user)
-		
-		#append and then prepare for new rows for follower's mutual followers
-		writer.writerow(out_list)
-		out_list = []
+                #start with original user's followers' ids
+                out_list = ids
+                out_list.insert(0, original_user)
+                
+                #append and then prepare for new rows for follower's mutual followers
+                writer.writerow(out_list)
+                out_list = []
 
-		#now that we wrote the original user, we can remove
-		#from the check since we already know the ids are friends of original_user
-		ids.remove(original_user)
+                #now that we wrote the original user, we can remove
+                #from the check since we already know the ids are friends of original_user
+                ids.remove(original_user)
 
-		rate_count = 0
+                rate_count = 0
 
-		for root in ids:
-			out_list = []
-			out_list.insert(0, root)
-			print("follower ", i+1, " of ", len(ids))
-			num_mutual = 0
-			
-			for node in ids:
-				rate_count += 1
+                for root in ids:
+                        out_list = []
+                        out_list.insert(0, root)
+                        print("follower ", i+1, " of ", len(ids))
+                        num_mutual = 0
+                        
+                        for node in ids:
+                                rate_count += 1
 
-				if root == node:
-					continue
+                                if root == node:
+                                        continue
 
-				while True:
-					try:
-						#check if user is friends
-						out = (api.show_friendship(source_id = root, target_id = node))
+                                while True:
+                                        try:
+                                                #check if user is friends
+                                                out = (api.show_friendship(source_id = root, target_id = node))
 
-						#rate_count = num of queries
-						#this if statement attempts to prevent hitting rate limit
-						if rate_count == 150:
-							print("Avoiding rate Limit at {0}: Waiting 15 mins".format(strftime("%H:%M:%S"), gmtime()))
-							time.sleep(900)
-							downtime += 900
-							rate_count = 0
+                                                #rate_count = num of queries
+                                                #this if statement attempts to prevent hitting rate limit
+                                                if rate_count == 150:
+                                                        print("Avoiding rate Limit at {0}: Waiting 15 mins".format(strftime("%H:%M:%S"), gmtime()))
+                                                        time.sleep(900)
+                                                        downtime += 900
+                                                        rate_count = 0
 
-					except tweepy.TweepError:
-						print("YOU HIT THE RATE LIMIT AT {0}: Waiting 15 mins".format(strftime("%H:%M:%S"), gmtime()))
-						time.sleep(900)
-						downtime += 900
-						continue
+                                        except tweepy.TweepError as e:
+                                                print("you hit an error!:")
+                                                print(e.api_code)
+                                                print(e.reason)
+                                                print("YOU HIT THE RATE LIMIT AT {0}: Waiting 15 mins".format(strftime("%H:%M:%S"), gmtime()))
+                                                time.sleep(900)
+                                                downtime += 900
+                                                continue
 
-					break
+                                        break
 
 
-				#if user is following = true
-				if(out[0].following):
-					num_mutual += 1
-					print("Bing")
-					out_list.append(node)
+                                #if user is following = true
+                                if(out[0].following):
+                                        num_mutual += 1
+                                        print("Bing")
+                                        out_list.append(node)
 
-			print()
-			print("writing to list")
-			writer.writerow(out_list)
-			print()
+                        print()
+                        print("writing to list")
+                        writer.writerow(out_list)
+                        print()
 
-			#this attempts to create a degree of connectivity by dividing total number of mutuals
-			#by the total number of root node followers
-			u_centrality = num_mutual / len(ids)
+                        #this attempts to create a degree of connectivity by dividing total number of mutuals
+                        #by the total number of root node followers
+                        u_centrality = num_mutual / len(ids)
 
-			centrality_list.append(u_centrality)     
-			i += 1
+                        centrality_list.append(u_centrality)     
+                        i += 1
 
-	file.close()
-	return(centrality_list, downtime)
+        file.close()
+        return(centrality_list, downtime)
 
 def main():
 
-	totaldowntime = 0
-	downtime = 0
-	start = time.time()
-	print("start time: {0}".format(strftime("%H:%M:%S"), gmtime()))
+        totaldowntime = 0
+        downtime = 0
+        start = time.time()
+        time.sleep(900)
+        print("start time: {0}".format(strftime("%H:%M:%S"), gmtime()))
 
-	nameFile = open("/home/higgins_colin/tweepy_followers/usernames", "r")
+        nameFile = open("/home/higgins_colin/tweepy_followers/usernames", "r")
 
-	for username in nameFile:
-		centrality_list = []
-		data = api.rate_limit_status()
-		x = data['resources']['followers']['/followers/ids']
-		rate_limit_left = x['remaining']
+        for username in nameFile:
+                centrality_list = []
+                data = api.rate_limit_status()
+                x = data['resources']['followers']['/followers/ids']
+                rate_limit_left = x['remaining']
 
-		username = username.strip()
+                username = username.strip()
 
-		#username = str(input("Enter a twitter username (no @):"))
-		original_user = username
-		
-		if(rate_limit_left == 1):
-			print("Follower ID check rate-limited at {0}: Waiting 15 mins".format(strftime("%H:%M:%S"), gmtime()))
-			time.sleep(900)
-			downtime += 900
+                #username = str(input("Enter a twitter username (no @):"))
+                original_user = username
+                
+                if(rate_limit_left == 1):
+                        print("Follower ID check rate-limited at {0}: Waiting 15 mins".format(strftime("%H:%M:%S"), gmtime()))
+                        time.sleep(900)
+                        downtime += 900
 
-		print(username)
-		try:
-			ids = get_followers(username)
+                print(username)
+                try:
+                        ids = get_followers(username)
 
-			#if the user has less than 250 followers, continue
-			if len(ids) <= 250:
-				centrality_list, downtime = check_connectivity(ids, original_user)
+                        #if the user has less than 250 followers, continue
+                        if len(ids) <= 150:
+                            centrality_list, downtime = check_connectivity(ids, original_user)
+                        else:
+                            continue
+                #if account privacy set to private
+                #skip account and print this message
+                except:
+                        print("THIS USERS ACCOUNT IS HIDDEN.")
 
 
-		#if account privacy set to private
-		#skip account and print this message
-		except:
-			print("THIS USERS ACCOUNT IS HIDDEN.")
+                totaldowntime += downtime
+                print(centrality_list)
+                print ("writing to analysis.csv")
+                with open("analysis.csv", 'a') as endfile:
+                        out_writer = csv.writer(endfile, delimiter= '|')
 
+                        centrality_list.insert(0, original_user)
+                        out_writer.writerow(centrality_list)
 
-		totaldowntime += downtime
-		print(centrality_list)
-		print ("writing to analysis.csv")
-		with open("analysis.csv", 'a') as endfile:
-			out_writer = csv.writer(endfile, delimiter= '|')
+        print("TOTAL TIME LOST TO API RATE LIMIT: ", totaldowntime/60, "MINUTES")
 
-			centrality_list.insert(0, original_user)
-			out_writer.writerow(centrality_list)
-
-	print("TOTAL TIME LOST TO API RATE LIMIT: ", totaldowntime/60, "MINUTES")
-
-	end = time.time()
-	print("\n elapsed time: ", end - start)
+        end = time.time()
+        print("\n elapsed time: ", end - start)
 
 main()
